fe457758d161368ca04dc32b42e7b1ec950677e6
Mallone <mallonenogueira@gmail.com>
2023-05-20 17:50:52 -0300

    change logo and favicon

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({})],
   test: {
     globals: true,
     environment: 'jsdom',
